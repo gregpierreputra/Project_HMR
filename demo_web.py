4a7a4cbf2e52c6bb665eb5c7ad197b75d3348200
I Putu Agi Karasugi <akar0104@student.monash.edu>
2025-08-04 21:45:58 +1000

    add gradio demo app

---
+++ demo_web.py
@@ -0,0 +1,23 @@
+import gradio as gr
+
+from demo_cli import HMRDemo, _parse_cli_args
+
+
+def _main():
+    args = _parse_cli_args(with_image_path=False)
+
+    inputs = [gr.Image(type="numpy", label="Input image")]
+
+    outputs = [
+        gr.Image(label="Output all mesh"),
+        gr.Model3D(label="3D Mesh"),
+    ]
+
+    hmr_demo = HMRDemo(args=args)
+
+    interface = gr.Interface(hmr_demo.run, inputs, outputs)
+    interface.launch()
+
+
+if __name__ == "__main__":
+    _main()
